996413a9771013f429900f88a58f4995ad3b8876
Mugilarasan <95951189+mugilmalathi@users.noreply.github.com>
2022-07-26 15:48:10 +0530

    file upload integration done

--- src/index.js
+++ src/index.js
@@ -2,6 +2,28 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+const fileupload = require('express-fileupload');
+
+app.use(fileupload());
+
+
+app.post('/upload', (req, res) => {
+    if(req.files === null) {
+        return res.status(400).json({msg: 'No file uploaded'});
+    }
+
+    const file = req.files.file;
+
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+        if(err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+
+        res.json({ fileName: file.name, filePath: `/uploads/${file.name}`});
+    })
+})
+
 
 app.use(cors());
 app.use(express.json());
